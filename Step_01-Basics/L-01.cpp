501cb40a58f35655de234ec0701b59b9cc250d1e
justinanurag <justinanurag0.2@gmail.com>
2025-05-11 11:32:56 +0530

    Stack Implementation using array

--- Step_01-Basics/L-01.cpp
+++ Step_01-Basics/L-01.cpp
@@ -1,5 +1,42 @@
 // Stack using Array 
-class st Impl{
+#include<iostream>
+#include<stack>
+using namespace std;
+class Stack {
+    int size;
+    int *arr;
     int top;
-    
+    public:
+    Stack(){
+        top=-1;
+        size=1000;
+        arr=new int[size];
+    }
+    void push(int x){
+        top++;
+        arr[top]=x;
+    }
+    int pop(){
+        int x=arr[top];
+        top--;
+        return x;
+    }
+    int Top(){
+        return arr[top];
+    }
+    int Size(){
+        return top+1;
+    }
+};
+int main(){
+    Stack s;
+    s.push(6);
+    s.push(7);
+    s.push(3);
+    cout<<"Top of stack before deleting any element"<<s.Top()<<endl;
+    cout << "Size of stack before deleting any element " << s.Size() << endl;
+  cout << "The element deleted is " << s.pop() << endl;
+  cout << "Size of stack after deleting an element " << s.Size() << endl;
+  cout << "Top of stack after deleting an element " << s.Top() << endl;
+  return 0;
 }

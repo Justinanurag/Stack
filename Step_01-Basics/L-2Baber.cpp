717236acdb3acb295e234cef8af1134da0582eca
justinanurag <justinanurag0.2@gmail.com>
2025-05-14 07:39:55 +0530

    Insert element at bottom of stack

--- Step_01-Basics/L-2Baber.cpp
+++ Step_01-Basics/L-2Baber.cpp
@@ -66,6 +66,7 @@ int main() {
     return 0;
 }
 */
+/*
 //Valid Parentesis
 #include<iostream>
 #include<stack>
@@ -108,6 +109,50 @@ int main() {
         cout << "Balanced" << endl;
     } else {
         cout << "Not Balanced" << endl;
+    }
+
+    return 0;
+}
+*/
+// Insert element at bottom of stack
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void solve(stack<int> &s, int x) {
+    // Base case
+    if(s.empty()) {
+        s.push(x);
+        return;
+    }
+
+    // Recursive case
+    int num = s.top();
+    s.pop();
+
+    solve(s, x);
+
+    // Push the element back after recursive call
+    s.push(num);
+}
+
+stack<int> pushAtBottom(stack<int>&s, int x) {
+    solve(s, x);
+    return s;
+}
+
+int main() {
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    s = pushAtBottom(s, 4);
+
+    while(!s.empty()) {
+        cout << s.top() << endl;
+        s.pop();
     }
 
     return 0;
